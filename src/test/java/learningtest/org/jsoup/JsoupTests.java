a5f0b1da1a3a374d688ddcb7727d0f202e2bbce5
Johnny Lim <izeye@naver.com>
2024-08-16 20:26:15 +0900

    Add more samples for jsoup

--- src/test/java/learningtest/org/jsoup/JsoupTests.java
+++ src/test/java/learningtest/org/jsoup/JsoupTests.java
@@ -1,12 +1,17 @@
 package learningtest.org.jsoup;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Tests for {@link Jsoup}.
@@ -16,7 +21,7 @@ import java.io.IOException;
 class JsoupTests {
 
     @Test
-    void test() throws IOException {
+    void connect() throws IOException {
         Document document = Jsoup.connect("https://en.wikipedia.org/wiki/Main_Page").get();
         System.out.println(document.title());
 
@@ -26,6 +31,45 @@ class JsoupTests {
             String url = headline.absUrl("href");
             System.out.println(title + ": " + url);
         }
+    }
+
+    @Test
+    void parse() throws IOException {
+        Document document = Jsoup.parse(new File("src/test/resources/thymeleaf.html"));
+        Elements elements = document.body().select("*");
+        for (Element element : elements) {
+            Attributes attributes = element.attributes();
+            for (Attribute attribute : attributes) {
+                String key = attribute.getKey();
+                String value = attribute.getValue();
+                System.out.println("Attribute: " + key + ": " + value);
+            }
+            System.out.println("Element: " + element);
+        }
+    }
+
+    @Test
+    void printAllThymeleafAttributeValues() throws IOException {
+        String path = "src/test/resources/thymeleaf.html";
+
+        Set<String> values = getAllThymeleafAttributeValues(path);
+        values.forEach(System.out::println);
+    }
+
+    private static Set<String> getAllThymeleafAttributeValues(String path) throws IOException {
+        Set<String> values = new TreeSet<>();
+        Document document = Jsoup.parse(new File(path));
+        Elements elements = document.body().select("*");
+        for (Element element : elements) {
+            Attributes attributes = element.attributes();
+            for (Attribute attribute : attributes) {
+                String value = attribute.getValue();
+                if (attribute.getKey().startsWith("th:")) {
+                    values.add(value);
+                }
+            }
+        }
+        return values;
     }
 
 }
